af33cc566ddb6dafacdd64d3de332705a35674fe
Ivan Y <ivany@subetuweb.site>
2022-05-03 15:35:38 -0500

    top nav

--- content/single.php
+++ content/single.php
@@ -1,16 +1,53 @@
-<div class="row align-items-center" style="height:600px;">
-    <div class="col">
-      
+<!-- user can change route by customizer -->
+<?php $image_attributes = wp_get_attachment_image_src( $attachment_id = 679 );
+  if ( $image_attributes ) : 
+    $image= $image_attributes[0];
+  endif;
+?> 
+
+<!-- floating image -->
+<img class="floating" src="<?= $image ?>">
+
+<!--  -->
+<div class="row principal" style="height:600px; ">
+
+  <div class="col-3 text-left">
+    <h1>
+      I'm<br>
+      <small class="">Ivan Yañez</small>
+    </h1>
+
+    <div class="row description">
+      <div class="col"><hr class="first"></div>      
+      <div class="col-8 animated">
+        <span is="type-async" id="type-text"></span>
+        <span class="blinking-cursor">_</span>
+      </div>
     </div>
-    <div class="col">
-      
+
+    <div class="row">
+      <p class="short">A frelancer who provides
+      services for digital
+      programming and
+      design content needs, 
+      for all bussines.</p>
     </div>
-    <div class="col">
-      
+
+  </div>
+
+  <div class="col d-flex align-items-center ">
+    <img class="align-self-center bio-image" src="http://localhost/wp-template-test/wp-content/uploads/2022/05/me-01.png" width=350 alt="pic">      
+  </div>
+
+  <div class="col-2"> <!-- top left -->
+    <div>
+      <p>Services</p>
     </div>
   </div>
+
 </div>
 
+<!-- services and blog -->
 <div class="row mx-5">
     <div class="col-9 px-5">
       One of three columns
@@ -62,3 +99,57 @@
 <div class="col container"  style="height:400px;">
    <p>Acerca</p>
 </div>
+
+<script>
+
+  const data = ['developer', 'fullstack', 'mobile'];
+
+  async function init () {
+
+    var i=0;
+
+    const node = document.querySelector("#type-text");
+  
+    await sleep(700);
+    
+    while (true) {      
+      await node.type(data[i]);
+      await sleep(2000);
+      await node.delete(data[i]);
+      if(i<data.length-1){              
+        i++;        
+      }else{
+        i=0;
+      }      
+      
+    }
+  }
+
+const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+
+class TypeAsync extends HTMLSpanElement {
+  get typeInterval () {
+    const randomMs = 100 * Math.random()
+    return randomMs < 50 ? 10 : randomMs
+  }
+  
+  async type (text) {
+    for (let character of text) {
+      this.innerText += character
+      await sleep(this.typeInterval)
+    }
+  }
+  
+  async delete (text) {
+    for (let character of text) {
+      this.innerText = this.innerText.slice(0, this.innerText.length -1)
+      await sleep(this.typeInterval)
+    }
+  }
+}
+
+customElements.define('type-async', TypeAsync, { extends: 'span' })
+
+init()
+
+</script>
